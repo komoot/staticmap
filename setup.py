5a7daf886a5332159c5439247bd47569a44011bf
Christoph Lingg <christoph@lingg.eu>
2015-11-13 15:15:13 +0100

    make project available via pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'staticmap',
+  packages = ['staticmap'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A small, python-based library for creating map images with lines and markers.',
+  author = 'Christoph Lingg',
+  author_email = 'christoph@komoot.de',
+  url = 'https://github.com/komoot/staticmap',
+  download_url = 'https://github.com/komoot/staticmap/tarball/0.1',
+  keywords = ['map', 'static map', 'map image', 'osm'],
+  classifiers = [],
+)
