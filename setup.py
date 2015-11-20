dfd585d5fa92cee1db562787a07053a120c369ae
Christoph Lingg <christoph@lingg.eu>
2015-11-20 17:17:33 +0100

    prepare next release with python2 support

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from distutils.core import setup
+
 setup(
-  name = 'staticmap',
-  packages = ['staticmap'],
-  version = '0.1',
-  description = 'A small, python-based library for creating map images with lines and markers.',
-  author = 'Christoph Lingg',
-  author_email = 'christoph@komoot.de',
-  url = 'https://github.com/komoot/staticmap',
-  download_url = 'https://github.com/komoot/staticmap/tarball/0.1',
-  keywords = 'static map image osm',
-  classifiers = [],
-  install_requires=[
-      'Pillow',
-      'requests'
-  ]
+    name='staticmap',
+    packages=['staticmap'],
+    version='0.2',
+    description='A small, python-based library for creating map images with lines and markers.',
+    author='Christoph Lingg',
+    author_email='christoph@komoot.de',
+    url='https://github.com/komoot/staticmap',
+    download_url='https://github.com/komoot/staticmap/tarball/0.1',
+    keywords='static map image osm',
+    classifiers=[],
+    install_requires=[
+        'Pillow',
+        'requests'
+    ]
 )
