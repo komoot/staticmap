96af5ff8956c14ae6feec3a6af31578cb64d8e9e
Christoph Lingg <christoph@lingg.eu>
2015-12-02 13:00:41 +0100

    pump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='staticmap',
     packages=['staticmap'],
-    version='0.4.2',
+    version='0.4.3',
     description='A small, python-based library for creating map images with lines and markers.',
     author='Christoph Lingg',
     author_email='christoph@komoot.de',
