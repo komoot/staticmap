f9a813bf3aa7173096fa31b1c3abf46606c90c81
Christoph Lingg <christoph@lingg.eu>
2015-12-02 23:06:47 +0100

    remove sleeping

--- staticmap/staticmap.py
+++ staticmap/staticmap.py
@@ -1,4 +1,3 @@
-import time
 from io import BytesIO
 from math import sqrt, log, tan, pi, cos, ceil, floor, atan, sinh
 
@@ -296,11 +295,9 @@ class StaticMap:
                     if res.status_code == 200:
                         break
 
-                    if nb_requests >= 4:
+                    if nb_requests >= 3:
                         # reached max tries to request tile
                         raise RuntimeError("could not download tile: {}: {}".format(self.url_template.format(z=self.zoom, x=x, y=y), res.status_code))
-
-                    time.sleep(3 ** nb_requests)  # 3,9,27,... seconds
 
                 tile = Image.open(BytesIO(res.content))
                 box = [
